3ad055b56baaa79faeabe75fc2d325e6a2bec1f7
Stefany Souza <stefany.cristinact88@gmail.com>
2019-09-21 17:19:35 -0300

    Aulas finais curso rest assured

---
+++ Rest Assured - Testes automatizado/leiloes-rest-assured/src/br/com/caelum/leilao/teste/UsuariosWSTest.java
@@ -0,0 +1,80 @@
+package br.com.caelum.leilao.teste;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.path.json.JsonPath;
+import com.jayway.restassured.path.xml.XmlPath;
+
+import br.com.caelum.leilao.modelo.Usuario;
+
+import static com.jayway.restassured.RestAssured.*;
+
+public class UsuariosWSTest {
+	private Usuario esperado1;
+	private Usuario esperado2;
+
+	@Before
+	public void setup() {
+		esperado1 = new Usuario(1L, "Mauricio Aniche", "mauricio.aniche@caelum.com.br");
+		esperado2 = new Usuario(2L, "Guilherme Silveira", "guilherme.silveira@caelum.com.br");
+	}
+
+	@Test
+	public void deveRetornarListaDeUsuarios() {
+		XmlPath path = given().header("accept", "application/xml").get("/usuarios?_format=xml").andReturn().xmlPath();
+
+		List<Usuario> usuarios = path.getList("list.usuario", Usuario.class);
+
+		assertEquals(esperado1, usuarios.get(0));
+		assertEquals(esperado2, usuarios.get(1));
+	
+	}
+
+	@Test
+	public void deveRetornarUsuarioPeloId() {
+		JsonPath path = given().parameter("usuario.id", 1).header("Accept", "application/json").get("/usuarios/show")
+				.andReturn().jsonPath();
+
+		Usuario usuario = path.getObject("usuario", Usuario.class);
+
+		assertEquals(esperado1, usuario);
+	}
+	
+	@Test
+    public void deveAdicionarUmUsuario() {
+        Usuario joao = new Usuario("Joao da Silva", "joao@dasilva.com");
+
+        XmlPath retorno = 
+            given()
+                .header("Accept", "application/xml")
+                .contentType("application/xml")
+                .body(joao)
+            .expect()
+                .statusCode(200)
+            .when()
+                .post("/usuarios")
+            .andReturn()
+                .xmlPath();
+
+        Usuario resposta = retorno.getObject("usuario", Usuario.class);
+
+        assertEquals("Joao da Silva", resposta.getNome());
+        assertEquals("joao@dasilva.com", resposta.getEmail());
+        
+        // deletando aqui
+        given()
+        	.contentType("application/xml").body(resposta)
+        .expect()
+        	.statusCode(200)
+        .when()
+        	.delete("/usuarios/deleta")
+        .andReturn()
+        	.asString();
+        }
+    }
